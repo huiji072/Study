a11295487a98fc064ed0dee5c3bcb69e28340eea
Your Name <you@example.com>
2022-09-05 18:27:06 +0900

    문자열 재정렬

--- CodingTestStudy/src/practice/UpDownLeftRightRe3.py
+++ CodingTestStudy/src/practice/UpDownLeftRightRe3.py
@@ -3,21 +3,20 @@
 
 n = int(input())
 x, y = 1, 1
-plans = input().split()
+datas = input().split()
 
 dx = [0, 0, -1, 1]
 dy = [-1, 1, 0, 0]
 move_types = ['L', 'R', 'U', 'D']
 
-for plan in plans:
+for data in datas:
     for i in range(len(move_types)):
-        if plan == move_types[i]:
-            nx = x + dx[i]
-            ny = y + dy[i]
-    
-    if nx  > n or ny > n or nx <  1 or ny < 1:
+        if data == move_types[i]:
+            nx = dx[i] + x
+            ny = dy[i] + y
+    if nx < 1 or ny < 1 or nx > n or ny > n:
         continue
+    
     x, y = nx, ny
     
-
 print(x, y)
