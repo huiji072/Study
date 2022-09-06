a1a439e88f54716db7783b8d925a0885ebcce888
Your Name <you@example.com>
2022-09-06 10:06:28 +0900

    chapter 07

--- reactStudy/my-app/src/index.js
+++ reactStudy/my-app/src/index.js
@@ -8,10 +8,11 @@ import Library from './chapter_03/Library';
 import Clock from './chapter_04/Clock';
 import CommentList from './chapter_05/CommentList';
 import NotificationList from './chapter_06/NotificationList';
+import Accomodate from './chapter_07/Accomodate';
 
 ReactDOM.render(
   <React.StrictMode>
-    <NotificationList />
+    <Accomodate />
   </React.StrictMode>,
   document.getElementById('root')
 );
