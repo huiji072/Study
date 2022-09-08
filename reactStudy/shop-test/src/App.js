fe6e94f2923d638056265cfe9c09086dab958356
Your Name <fmti0720@gmail.com>
2022-09-08 23:48:54 +0900

    기본 login 화면

---
+++ reactStudy/shop-test/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import TextField from '@mui/material/TextField';
+import Checkbox from '@mui/material/Checkbox';
+import Button from '@mui/material/Button';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Login from './login/Login';
+
+function App() {
+  return (
+    <Login></Login>
+  );
+}
+
+export default App;
---
+++ reactStudy/shop-test/src/login/Login.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import TextField from '@mui/material/TextField';
+import Checkbox from '@mui/material/Checkbox';
+import Button from '@mui/material/Button';
+import FormControlLabel from '@mui/material/FormControlLabel';
+
+class Login extends Component {
+    render() {
+        return(
+            <div className="Login">
+            <FormControlLabel 
+              control={<Checkbox value="기부자"/>}
+              label="기부자"
+            />
+            <FormControlLabel 
+              control={<Checkbox value="일반회원"/>}
+              label="일반회원"
+            />
+            <br></br>
+            <TextField 
+              label="Email Address" 
+              required 
+              name="email"
+              autoComplete="email"
+              autoFocus
+            /> 
+            <br></br>
+            <TextField 
+              label="Password" 
+              type="password" 
+              required
+              name="password"
+              autoComplete="current-password"
+            />
+            <br></br>
+            <FormControlLabel 
+              control={<Checkbox value="remember"/>}
+              label="remember"
+            />
+            <Button type='submit' variant="contained" sx={{ mt: 3, mb: 2}} >Sign in</Button>
+          </div>
+        );
+    }
+}
+
+export default Login;
