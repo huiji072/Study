dbaef80ce8648199d92760dd4ac379a167b04cc2
Your Name <fmti0720@gmail.com>
2022-09-13 00:04:27 +0900

    판매이력ui 수정

--- reactStudy/shop-test/src/cart/CartList.jsx
+++ reactStudy/shop-test/src/cart/CartList.jsx
@@ -8,36 +8,58 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 class CartList extends Component {
     render() {
         return(
-            <div class="cartList">
-            <div class="content-mg">
-                <h2 class="mb-4">장바구니 목록</h2>
-
-            <div>
+            <div class="cartContainer">
+                <div>
+                    <h2 class="mb-4">장바구니 목록</h2>
+                <div>
 
                 <table class="table">
-                    <colgroup>
-                        <col width="30%"/>
-                        <col width="70%"/>
-                    </colgroup>
                     <thead>
-                    <tr class="text-center">
+                    <tr class="tr1">
                         <td>
-                            <input type="checkbox" id="checkall" onclick="checkAll()"/> 전체선택
+                            <input type="checkbox" id="checkall" onclick="checkAll()"/> 
                         </td>
                         <td>상품정보</td>
                     </tr>
                     </thead>
                     <tbody>
+
                     <tr>
                         <td class="text-center align-middle">
                             <input type="checkbox" name="cartChkBox" />
                         </td>
                         <td class="d-flex">
-                            <div class="repImgDiv align-self-center">
-                                <img src='img/bed.jpg' class = "rounded repImg"/>
+                            <div >
+                                <img src='/img/bed.jpg' class = "cartImg"/>
                             </div>
                             <div class="align-self-center">
-                                <span class="fs24 font-weight-bold"></span>
+                                <span class="fs24 font-weight-bold">[상품명] 길이 테스트입니다.</span>
+                                <div class="fs18 font-weight-light">
+                                    <span class="input-group mt-2">
+
+                                        <input type="number" name="count" 
+                                             min="1"
+                                            onchange="changeCount(this)" class="form-control mr-2" />
+                                        <button type="button" class="close" aria-label="Close">
+                                            <span aria-hidden="true" onclick="deleteCartItem(this)">&times;</span>
+                                        </button>
+                                    </span>
+                                </div>
+                            </div>
+                        </td>
+
+                    </tr>
+
+                    <tr>
+                        <td class="text-center align-middle">
+                            <input type="checkbox" name="cartChkBox" />
+                        </td>
+                        <td class="d-flex">
+                            <div >
+                                <img src='/img/bed.jpg' class = "cartImg"/>
+                            </div>
+                            <div class="align-self-center">
+                                <span class="fs24 font-weight-bold">[상품명]</span>
                                 <div class="fs18 font-weight-light">
                                     <span class="input-group mt-2">
 
@@ -61,8 +83,8 @@ class CartList extends Component {
                     <button type="button" class="btn btn-primary btn-lg" onclick="orders()">주문하기</button>
                 </div>
 
+                </div>
             </div>
-        </div>
 
             </div>
         );
--- reactStudy/shop-test/src/offer/OfferHist.jsx
+++ reactStudy/shop-test/src/offer/OfferHist.jsx
@@ -4,20 +4,17 @@ import './OfferHist.css';
 class OfferHist extends Component{
     render() {
         return(
-            <>
-            <div class="content-mg">
-
-            <h2 class="mb-4">
-                판매 이력
-            </h2>
-
+            <div class="containerOfferHist">
+                
+                
+            <h2 class="mb-4">판매 이력</h2>
             <div>
 
                 <div class="d-flex mb-3 align-self-center">
-                    <h4 >2022-08-25 19:16 주문 </h4>
+                    <h4 >2022-08-25 19:16 주문  </h4>
                     <div class="ml-3">
                         <th>
-                            <button type="button" class="btn btn-outline-secondary"  onclick="cancelOrder(this.value)">주문취소</button>
+                            <button type="button" class="btn btn-outline-secondary"  onclick="cancelOrder(this.value)"> 주문취소</button>
                         </th>
                         {/* <th >
                             <h4>(취소 완료)</h4>
@@ -26,14 +23,50 @@ class OfferHist extends Component{
                 </div>
 
                 
-                <div class="card d-flex">
+                <div class="cardOfferHist">
                     <div class="d-flex mb-3">
 
                         <div class="repImgDiv">
-                            <img src='img/bed.jpg' class = "rounded repImg" />
+                            <img src='/img/sopa.jpg' class = "imgOfferHist" />
                         </div>
 
-                        <div class="align-self-center w-75">
+                        <div class="contentsOfferHist">
+                            <span  class="fs24 font-weight-bold">ItemName</span>
+                            <div class="fs18 font-weight-light">
+                                <span>itemCount</span>
+                            </div>
+                            <div class="fs18 font-weight-light">
+                                <span>구매자 </span>
+                                <span >buyerName</span>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+                <br/>
+
+
+                <div class="d-flex mb-3 align-self-center">
+                    <h4 >2022-08-25 19:16 주문  </h4>
+                    <div class="ml-3">
+                        <th>
+                            <button type="button" class="btn btn-outline-secondary"  onclick="cancelOrder(this.value)"> 주문취소</button>
+                        </th>
+                        {/* <th >
+                            <h4>(취소 완료)</h4>
+                        </th> */}
+                    </div>
+                </div>
+
+                
+                <div class="cardOfferHist">
+                    <div class="d-flex mb-3">
+
+                        <div class="repImgDiv">
+                            <img src='/img/sopa.jpg' class = "imgOfferHist" />
+                        </div>
+
+                        <div class="contentsOfferHist">
                             <span  class="fs24 font-weight-bold">ItemName</span>
                             <div class="fs18 font-weight-light">
                                 <span>itemCount</span>
@@ -47,30 +80,11 @@ class OfferHist extends Component{
                     </div>
                 </div>
 
-                <div class="card d-flex">
-                    <div class="d-flex mb-3">
-                        <div class="repImgDiv">
-                            <img src='img/bed.jpg' class = "rounded repImg" />
-                        </div>
-                        <div class="align-self-center w-75">
-                            <span  class="fs24 font-weight-bold">ItemName</span>
-                            <div class="fs18 font-weight-light">
-                                <span>itemCount</span>
-                            </div>
-                            <div class="fs18 font-weight-light">
-                                <span>판매자 </span>
-                                <span >sellerName</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-
             </div>
 
 
-            </>
+
+            </div>
         );
     }
 }
