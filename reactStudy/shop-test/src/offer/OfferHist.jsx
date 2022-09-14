18697564d2aed2333238e8526554938f1a906862
Your Name <fmti0720@gmail.com>
2022-09-14 16:44:18 +0900

    판매이력 props 적용

--- reactStudy/shop-test/src/offer/OfferHist.jsx
+++ reactStudy/shop-test/src/offer/OfferHist.jsx
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import './OfferHist.css';
 
-class OfferHist extends Component{
-    render() {
+function OfferHist(props) {
         return(
             <div class="containerOfferHist">
                 
@@ -11,7 +10,7 @@ class OfferHist extends Component{
             <div>
 
                 <div class="d-flex mb-3 align-self-center">
-                    <h4 >2022-08-25 19:16 주문  </h4>
+                    <h4 >{props.regTime}  </h4>
                     <div class="ml-3">
                         <th>
                             <button type="button" class="btn btn-outline-secondary"  onclick="cancelOrder(this.value)"> 주문취소</button>
@@ -31,13 +30,13 @@ class OfferHist extends Component{
                         </div>
 
                         <div class="contentsOfferHist">
-                            <span  class="fs24 font-weight-bold">ItemName</span>
+                            <span  class="fs24 font-weight-bold">{props.itemNm}</span>
                             <div class="fs18 font-weight-light">
-                                <span>itemCount</span>
+                                <span>{props.stockNumber}</span>
                             </div>
                             <div class="fs18 font-weight-light">
                                 <span>구매자 </span>
-                                <span >buyerName</span>
+                                <span >{props.createdBy}</span>
                             </div>
                         </div>
                         
@@ -45,48 +44,9 @@ class OfferHist extends Component{
                 </div>
                 <br/>
 
-
-                <div class="d-flex mb-3 align-self-center">
-                    <h4 >2022-08-25 19:16 주문  </h4>
-                    <div class="ml-3">
-                        <th>
-                            <button type="button" class="btn btn-outline-secondary"  onclick="cancelOrder(this.value)"> 주문취소</button>
-                        </th>
-                        {/* <th >
-                            <h4>(취소 완료)</h4>
-                        </th> */}
-                    </div>
-                </div>
-
-                
-                <div class="cardOfferHist">
-                    <div class="d-flex mb-3">
-
-                        <div class="repImgDiv">
-                            <img src='/img/sopa.jpg' class = "imgOfferHist" />
-                        </div>
-
-                        <div class="contentsOfferHist">
-                            <span  class="fs24 font-weight-bold">ItemName</span>
-                            <div class="fs18 font-weight-light">
-                                <span>itemCount</span>
-                            </div>
-                            <div class="fs18 font-weight-light">
-                                <span>구매자 </span>
-                                <span >buyerName</span>
-                            </div>
-                        </div>
-                        
-                    </div>
-                </div>
-
             </div>
-
-
-
             </div>
         );
-    }
 }
 
 export default OfferHist;
