c76824333663be74926ef8c371db20863375a53e
Your Name <fmti0720@gmail.com>
2022-09-16 13:16:31 +0900

    main ui 수정

--- reactStudy/shop-test/src/main/MainList.jsx
+++ reactStudy/shop-test/src/main/MainList.jsx
@@ -4,45 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container ,Card,Row, Col, Button} from 'react-bootstrap';  
 import './Main.css';
 
-const itemList = [
-    {
-        itemId: 1,
-        itemNm: "test1",
-        itemDetail: "props를 이용하여 출력된 값입니다.",
-        stockNumber: 10,
-        itemSellStatus: "SELL",
-        createdBy: "seller1@gmail.com",
-        regTime: "2022-08-25"
-    },
-    {
-        itemId: 2,
-        itemNm: "test2",
-        itemDetail: "props를 이용하여 출력된 값입니다2!!",
-        stockNumber: 200,
-        itemSellStatus: "SOLD_OUT",
-        createdBy: "seller1@gmail.com",
-        regTime: "2022-08-230"
-    },
-    {
-        itemId: 3,
-        itemNm: "test2",
-        itemDetail: "props를 이용하여 출력된 값입니다2!!",
-        stockNumber: 200,
-        itemSellStatus: "SOLD_OUT",
-        createdBy: "seller1@gmail.com",
-        regTime: "2022-08-230"
-    },
-    {
-        itemId: 4,
-        itemNm: "test2",
-        itemDetail: "props를 이용하여 출력된 값입니다2!!",
-        stockNumber: 200,
-        itemSellStatus: "SOLD_OUT",
-        createdBy: "seller1@gmail.com",
-        regTime: "2022-08-230"
-    }
-]
-
 function MainList() {
 
     const [ testStr, setTestStr ] = useState('');
@@ -70,8 +31,8 @@ function MainList() {
                             <a href='item/ItemDtl'>
                                 <img className="card-img-top" src='/img/bed.jpg' />
                                 <div className="card-body">
-                                    <h4 className="card-title">{item.itemNm}</h4>
-                                    <p className="card-text">{item.itemDetail}</p>
+                                    <h4 className="card-title">{item.name}</h4>
+                                    <p className="card-text">{item.dtl}</p>
                                     <h3 class="card-title text-danger">{item.stockNumber}</h3>
                                 </div>
                             </a>
