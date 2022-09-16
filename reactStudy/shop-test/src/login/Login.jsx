d2e15f37aaae1b875126960fea41932964ecd922
Your Name <fmti0720@gmail.com>
2022-09-16 16:38:59 +0900

    login-email, password alert창에 띄우기

--- reactStudy/shop-test/src/login/Login.jsx
+++ reactStudy/shop-test/src/login/Login.jsx
@@ -1,53 +1,68 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './Login.css';
-import TextField from '@mui/material/TextField';
-import Checkbox from '@mui/material/Checkbox';
-import Button from '@mui/material/Button';
-import FormControlLabel from '@mui/material/FormControlLabel';
 
-class Login extends Component {
-    render() {
-        return(
-            <div class="loginContainer">
-            <h2 class="mb-4">로그인</h2>
-                 <form class="loginForm">
-                    <div className="mb-3">
-                      <label>Email address</label>
-                      <input
-                        type="email"
-                        className="form-control"
-                        placeholder="Enter email"
-                      />
-                    </div>
-                    <div className="mb-3">
-                      <label>Password</label>
-                      <input
-                        type="password"
-                        className="form-control"
-                        placeholder="Enter password"
-                      />
-                    </div>
-                    <div className="mb-3">
-                      <div className="custom-control custom-checkbox">
-                        <input
-                          type="checkbox"
-                          className="custom-control-input"
-                          id="customCheck1"
-                        />
-                        <label className="custom-control-label" htmlFor="customCheck1">
-                          Remember me
-                        </label>
-                      </div>
-                    </div>
-                    <div className="d-grid">
-                      <button type="submit" className="btn btn-primary">
-                        Submit
-                      </button>
-                    </div>
-                  </form>
-          </div>
-        );
-    }
+function Login() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleChangeEmail = (event) => {
+    setEmail(event.target.value);
+  }
+
+  const handleChangePassword = (event) => {
+    setPassword(event.target.value);
+  }
+
+  const handleChangeLoginSubmit = (event) => {
+    alert(`email :  ${email}`)
+    event.preventDefault();
+  }
+
+  return(
+    <div class="loginContainer">
+    <h2 class="mb-4">로그인</h2>
+         <form class="loginForm" onSubmit={handleChangeLoginSubmit}>
+            <div className="mb-3">
+              <label>Email address</label>
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Enter email"
+                value={email}
+                onChange={handleChangeEmail}
+              />
+            </div>
+            <div className="mb-3">
+              <label>Password</label>
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Enter password"
+                value={password}
+                onChange={handleChangePassword}
+              />
+            </div>
+            <div className="mb-3">
+              <div className="custom-control custom-checkbox">
+                <input
+                  type="checkbox"
+                  className="custom-control-input"
+                  id="customCheck1"
+                />
+                <label className="custom-control-label" htmlFor="customCheck1">
+                  Remember me
+                </label>
+              </div>
+            </div>
+            <div className="d-grid">
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </div>
+          </form>
+  </div>
+);      
 }
 
 export default Login;
