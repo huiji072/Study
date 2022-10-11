7c532e594eddeaca7cbbe589daa04edf45aa293b
Your Name <fmti0720@gmail.com>
2022-10-11 23:20:30 +0900

    ch11

--- Study-3b7dba95a56964c6aba99728f209a7ae3baec1bd/reactStudy/shop-test/src/test/ApiService.jsx
+++
@@ -1,9 +0,0 @@
-// import React from "react";
-
-// export function signin(Member) {
-//     return callback("/auth/signin", "POST", Member)
-//     .then((response) => {
-//         console.log("response: ", response);
-//         alert("로그인 토큰: " + response.token);
-//     });
-// }
