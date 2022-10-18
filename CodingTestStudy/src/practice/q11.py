4617701e565a616334ccca405f31d8ab202f42ae
Your Name <fmti0720@gmail.com>
2022-10-18 23:54:15 +0900

    q11

--- CodingTestStudy/src/practice/q11.py
+++ CodingTestStudy/src/practice/q11.py
@@ -1,35 +1,42 @@
+from time import time
+
+
 n = int(input())
 k = int(input())
 
-data = [[0] * (n+1) for _ in range(n+1)]
-info = []
+# n*n 기본 값은 0
+data = [[0] * (n+1) in range(n+1)]
 
+# 사과가 있는 곳은 1
 for _ in range(k):
     a, b = map(int, input().split())
     data[a][b] = 1
-
-l = int(input())
-
-for i in range(l):
-    a, b = input().split()
-    info.append((int(a), b))
     
+    
+l = int(input())
+info = []
+for _ in range(l):
+    x, c = input().split()
+    info.append((int(x), c))
+    
+
+direction = 0    
 dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
 
 def turn(direction, c):
-    if c == "L":
+    if c == 'L':
         direction = (direction - 1) % 4
     else:
-        direction = (direction +  1) % 4
+        direction = (direction + 1) % 4
     return direction
 
-def simulate():
+def solution():
     x, y = 1, 1
     data[x][y] = 2
-    direction = 0
-    time = 0
     index = 0
+    temp = 0
+    direction = 0
     
     q = [(x, y)]
     
@@ -37,21 +44,20 @@ def simulate():
         nx = x + dx[direction]
         ny = y + dy[direction]
         
-        if 1 <= nx and nx <= n and 1 <= ny and ny <= n and data[nx][ny] != 2:
-            if data[nx][ny] == 0: #사과가 없을 때
-                data[nx][ny] = 2
-            
-            if data[nx][ny] == 1:
-                data[nx][ny]
-                q.append((nx, ny))
-        else:
+        if nx < 1 or ny < 1 or nx > n or ny > n:
             time += 1
             break
+        else:
+            if data[nx][ny] == 0:
+                data[nx][ny] = 2
+            if data[nx][ny] == 1:
+                data[nx][ny] = 2
+                q.append((nx, ny))
         x, y = nx, ny
         time += 1
-        if index < 1 and time == info[index][0]:
-            direction = turn(direction, info[index][1])
+        
+        if time == data[index][0]:
+            turn(direction, data[index][1])
             index += 1
     return time
-        
-print(simulate())
+                
