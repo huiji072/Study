4ba1083a0cec5e4f5b539bdecc7626daac4f7865
Your Name <fmti0720@gmail.com>
2022-10-21 23:04:35 +0900

    2960백준

--- CodingTestStudy/src/baekjoon/2960.py
+++ CodingTestStudy/src/baekjoon/2960.py
@@ -2,10 +2,10 @@ import math
 
 n, k = map(int, input().split())
 
-data = [True for i in range(n+1)]
+data = [True] * (n+1)
 
 temp = 0
-for i in range(2, int(math.sqrt(n)) + 1):
+for i in range(2, n + 1):
     for j in range(i, n+1, i):
         if data[j] == True:
             data[j] = False
