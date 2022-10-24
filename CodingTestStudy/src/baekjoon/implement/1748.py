9d59bf039c639298f89582f0ee9357bb8644489a
Your Name <fmti0720@gmail.com>
2022-10-24 21:54:57 +0900

    구현4 1748

--- CodingTestStudy/src/baekjoon/implement/1748.py
+++ CodingTestStudy/src/baekjoon/implement/1748.py
@@ -1,1 +1,12 @@
 # 구현4
+
+n = input()
+
+n_len = len(n) - 1
+
+result = 0
+for i in range(n_len):
+    result += 9 * 10 ** i * (i+1)
+
+result += (int(n) -  (10 ** n_len) + 1) * (n_len+1)
+print(result)
