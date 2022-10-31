11b70410ae780096e16ccd1394039971803d3451
Your Name <fmti0720@gmail.com>
2022-10-31 21:58:44 +0900

    bfs/dfs

---
+++ CodingTestStudy/src/baekjoon/bfs_dfs/11724.py
@@ -0,0 +1,29 @@
+n, m = map(int, input().split())
+
+graph = [[] for _ in range(n+1)]
+
+visited =  [False] * (n+1)
+
+for i in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+        
+for i in graph:
+    i.sort()
+    
+def dfs(v):
+    visited[v] = True
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(i)
+    
+result = 0
+for i in range(1, n+1):
+    if not visited[i]:
+        dfs(i)
+        result += 1
+        
+        
+            
+print(result)
