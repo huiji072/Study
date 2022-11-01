e369813b442d8788f09a357e3e60f3f6c5d25ab0
Your Name <fmti0720@gmail.com>
2022-11-01 21:06:45 +0900

    이분정렬1

--- CodingTestStudy/src/baekjoon/bfs_dfs/11724.py
+++ CodingTestStudy/src/baekjoon/bfs_dfs/11724.py
@@ -1,3 +1,7 @@
+import sys
+
+
+sys.setrecursionlimit(10000)
 n, m = map(int, input().split())
 
 graph = [[] for _ in range(n+1)]
@@ -8,9 +12,6 @@ for i in range(m):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
-        
-for i in graph:
-    i.sort()
     
 def dfs(v):
     visited[v] = True
---
+++ CodingTestStudy/src/thisiscodingtest/ch07/q02.py
@@ -0,0 +1,32 @@
+n = int(input())
+
+# 존재하는 데이터
+data = list(map(int, input().split()))
+data.sort()
+
+m = int(input())
+# 필요한 데이터
+order_data = list(map(int, input().split()))
+
+def binary_search(data, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        # 가운데와 타겟이 같을 경우
+        if data[mid] == target:
+            return mid
+        # 가운데보다 타겟이 더 작을 경우
+        elif data[mid] > target:
+            end = mid - 1
+        # 가운데보다 타겟이 더 클 경우
+        else:
+            start = mid + 1
+    return None
+ 
+for i in order_data:
+    result = binary_search(data, i, 0, n-1)
+    # 타겟이 data에 있는 경우
+    if result != None:
+        print('yes', end=' ')
+    # 타겟이 data에 없는 경우
+    else:
+        print('no', end=' ')
