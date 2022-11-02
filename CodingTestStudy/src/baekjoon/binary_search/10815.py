51ae0e3ed342939428ed7611f395a7cd4a3c60a0
Your Name <fmti0720@gmail.com>
2022-11-02 20:48:32 +0900

    이분정렬 1

---
+++ CodingTestStudy/src/baekjoon/binary_search/10815.py
@@ -0,0 +1,24 @@
+n = int(input())
+data = list(map(int, input().split()))
+data.sort()
+m = int(input())
+target_data = list(map(int, input().split()))
+
+def binary_search(data, target, start, end):
+    while start <= end:
+        mid = (start+end) // 2
+        
+        if data[mid] == target:
+            return mid
+        elif data[mid] > target:
+            end = mid - 1
+        elif data[mid] < target:
+            start = mid + 1
+    return None
+
+for i in target_data:
+    result = binary_search(data, i, 0, n-1)
+    if result != None:
+        print(1, end=' ')
+    else:
+        print(0, end=' ')
