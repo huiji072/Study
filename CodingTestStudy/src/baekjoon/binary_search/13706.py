4079e762362a2e2276ef8227d37c2e7206a86cbc
Your Name <fmti0720@gmail.com>
2022-11-02 21:05:07 +0900

    이분정렬 2

--- CodingTestStudy/src/baekjoon/binary_search/13706.py
+++ CodingTestStudy/src/baekjoon/binary_search/13706.py
@@ -3,17 +3,14 @@ n = int(input())
 start = 0
 end = n
 
-def binary_search(target, start, end):
-    while start <= end:
-        mid = (start+end) // 2
+while start <= end:
+    mid = (start+end) // 2
         
-        if mid**2 == target:
-            return mid
-        elif mid**2 > target:
-            end = mid - 1
-        else:
-            start = mid + 1
+    if mid**2 == n:
+        print(mid)
+        break
+    elif mid**2 > n:
+        end = mid - 1
+    else:
+        start = mid + 1
     
-for i in range(n):
-    result = binary_search(n, 0, n-1)
-print(result)
